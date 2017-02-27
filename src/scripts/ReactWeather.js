122540c4bd3f5b566141ca057e42bbe0dafd7482
Ahmed Farahat <farahat80@gmail.com>
2017-02-27 16:27:27 +0100

    initial commit

---
+++ src/scripts/ReactWeather.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from '../styles/react-weather.scss';
+
+class ReactWeather extends React.Component {
+  render() {
+    return (<p>Hello World Component.</p>);
+  }
+}
+
+export default ReactWeather;
