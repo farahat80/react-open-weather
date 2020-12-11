f592b5fce6edd990057b256c58c8943183898c55
David Rubert <david.rubert@gmail.com>
2020-12-11 19:24:05 +0100

    Added the Catalan language translation

--- src/js/lang.js
+++ src/js/lang.js
@@ -1,5 +1,6 @@
 export const langText = {
   en: { wind: 'Wind', humidity: 'Humidity' },
   es: { wind: 'Viento', humidity: 'Humedad' },
+  ca: { wind: 'Vent', humidity: 'Humitat' },
   de: { wind: 'Windstärke', humidity: 'Feuchtigkeit' },
 };
