365b5db35fd70f2c4bbdb0abe3bd28f221db8a33
carie8655 <carie8655@gmail.com>
2021-01-13 21:37:37 +0800

    update - add chinese traditional

--- src/js/lang.js
+++ src/js/lang.js
@@ -1,5 +1,6 @@
 export const langText = {
   en: { wind: 'Wind', humidity: 'Humidity' },
   es: { wind: 'Viento', humidity: 'Humedad' },
+  zh_tw: { wind: '風速', humidity: '濕度' },
   de: { wind: 'Windstärke', humidity: 'Feuchtigkeit' },
 };
