5f6701e3c4cdd6121d3259f1aa264a2dfb24a2e3
Ahmed Farahat <farahat80@gmail.com>
2021-09-30 15:42:20 +0200

    ISSUE-72: added fixed class names to ease css override

--- src/js/components/Forecast.styles.js
+++ src/js/components/Forecast.styles.js
@@ -10,6 +10,7 @@ const useStyles = createUseStyles({
     borderRadius: '0 0 5px 5px',
     fontSize: 11,
     backgroundColor: ({ theme }) => theme.forecastBackgroundColor,
+    composes: 'rw-forecast-days-panel',
   },
   day: {
     width: '25%',
@@ -19,23 +20,28 @@ const useStyles = createUseStyles({
     '&:not(:first-child)': {
       borderLeft: ({ theme }) => `solid 1px ${theme.forecastSeparatorColor}`,
     },
+    composes: 'rw-forecast-day',
   },
   date: {
     color: ({ theme }) => theme.forecastDateColor,
     fontSize: 11,
     fontWeight: 'bold',
+    composes: 'rw-forecast-date',
   },
   desc: {
     color: ({ theme }) => theme.forecastDescColor,
     margin: [10, 0, 10, 0],
     fontSize: 12,
+    composes: 'rw-forecast-desc',
   },
   range: {
     color: ({ theme }) => theme.forecastRangeColor,
     fontSize: 11,
+    composes: 'rw-forecast-range',
   },
   icon: {
     paddingTop: 10,
+    composes: 'rw-forecast-icon',
   },
 });
 
--- src/js/components/Today.styles.js
+++ src/js/components/Today.styles.js
@@ -3,25 +3,30 @@ import { createUseStyles } from 'react-jss';
 const useStyles = createUseStyles({
   date: {
     color: ({ theme }) => theme.todayDateFontColor,
+    composes: 'rw-today-date',
   },
   current: {
     fontSize: 45,
     color: ({ theme }) => theme.todayTempFontColor,
+    composes: 'rw-today-current',
   },
   range: {
     color: ({ theme }) => theme.todayRangeFontColor,
     fontSize: 12,
     margin: [0, 0, 5, 2],
+    composes: 'rw-today-range',
   },
   desc: {
     color: ({ theme }) => theme.todayDescFontColor,
     fontSize: 16,
+    composes: 'rw-today-desc',
   },
   info: {
     color: ({ theme }) => theme.todayInfoFontColor,
     '& div': {
       marginBottom: 5,
     },
+    composes: 'rw-today-info',
   },
   hr: {
     width: '100%',
@@ -29,6 +34,7 @@ const useStyles = createUseStyles({
     borderBottom: 'solid 1px #fff',
     opacity: '0.4',
     margin: [10, 0],
+    composes: 'rw-today-hr',
   },
 });
 
