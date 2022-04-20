2d02876e02ee99657e421b9eb45312a7248712d6
Rasmus Pedersen <rollo2701@gmail.com>
2022-04-20 14:44:20 +0200

    Update lang.js
    Added Danish language support in lang.js

--- src/js/lang.js
+++ src/js/lang.js
@@ -11,6 +11,7 @@ import 'dayjs/locale/fr';
 import 'dayjs/locale/it';
 import 'dayjs/locale/pl';
 import 'dayjs/locale/tr';
+import 'dayjs/locale/da';
 
 export const langText = {
   ca: { wind: 'Vent', humidity: 'Humitat' },
@@ -27,4 +28,5 @@ export const langText = {
   it: { wind: 'Vento', humidity: 'Umidità' },
   pl: { wind: 'Wiatr', humidity: 'Wilgotność powietrza' },
   tr: { wind: 'Rüzgar', humidity: 'Nem Oranı' },
+  da: { wind: 'Vind', humidity: 'Luftfugtighed' },
 };
